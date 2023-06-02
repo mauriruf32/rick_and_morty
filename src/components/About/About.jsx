d930fb6041cbe883cd0b0c79266f68ed5b6eecac
mauriruf32 <mauriruffini32@gmail.com>
2023-06-02 14:40:28 -0300

    Integration Homework

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,8 @@ const About = () => {
   return (
     <div className={styles.conteiner}>
 
-      <h1>Hola soy Mauricio</h1>
+      <h1>Hola, mi nombre es Mauricio y espero esten disfrutando de mi App de Rick and Morty.</h1>
+      <h2>Esta App, aunque tal vez no lo parezca a primera vista, implico mucho esfuerzo, dedicacion y estudio, y tengo que agradecer a la oportunidad que SoyHenry me esta dando con esta alternativa de cursado online.</h2>
       
     </div>
 
