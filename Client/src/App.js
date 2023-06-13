9a44e80d8dbc993db10c5a0bbf94582fa572c2ff
mauriruf32 <mauriruffini32@gmail.com>
2023-06-12 22:52:21 -0300

    Integration WebServer

---
+++ Client/src/App.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { ROUTES } from './helpers/RoutesPath';
+import axios from 'axios';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import Cards from './components/Cards/Cards.jsx';
+import Nav from './components/Nav/Nav.jsx';
+import About from './components/About/About.jsx';
+import Form from './components/Form/Form';
+import Detail from './components/Detail/Detail';
+import Favorites from './components/Favorites/Favorites';
+
+
+function App() {
+
+   const { pathname } = useLocation()
+   const [characters, setCharacters] = useState([])
+   const navigate = useNavigate();
+   const [access, setAccess] = useState(false);
+   const EMAIL = '';
+   const PASSWORD = '';
+
+   function login(userData) {
+      if (userData.password === PASSWORD && userData.email === EMAIL) {
+         setAccess(true);
+         navigate('/home');
+      }
+   }
+
+   const onSearch = (id) => {
+      axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose = (id) => {
+      setCharacters(characters.filter((char) => {
+         return char.id !== Number(id)
+      }))
+   }
+
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
+
+   return (
+      <div className='App'>
+         {pathname !== '/' && <Nav onSearch={onSearch} />}
+         <Routes>
+            <Route path={'/'} element={<Form login={login} />} />
+            <Route path={'/home'} element={<Cards characters={characters} onClose={onClose} />} />
+            <Route path={'/about'} element={<About />} />
+            <Route path='/favorites' element={<Favorites />} />
+            <Route path='/detail/:id' element={<Detail />} />
+         </Routes>
+
+      </div>
+   );
+}
+
+export default App;
---
+++ Client/src/components/Detail/Detail.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import styles from "./Detail.module.css";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const Detail = () => {
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    axios(`http://localhost:3001/rickandmorty/character/${id}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacter(data);
+        } else {
+          window.alert("No hay personajes con ese ID");
+        }
+      }
+    );
+    return setCharacter({});
+  }, [id]);
+
+  return (
+    <div className={styles.detail}>
+      <h1>{character?.name}</h1>
+      <h2>{character?.status}</h2>
+      <h2>{character?.species}</h2>
+      <h2>{character?.gender}</h2>
+      <h2>{character.origin?.name}</h2>
+      <img className={styles.detailImg}src={character.image} alt="char detail" />
+    </div>
+  );
+};
+
+export default Detail;
